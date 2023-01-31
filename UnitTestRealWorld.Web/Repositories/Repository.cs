a69e829f8ec3a70384d439dcfae52c4b5b39bb16
Metin Çelebi <metincelebi5534@gmail.com>
2023-01-31 14:20:50 +0300

    Test edilecek .NET Core projesi oluşturuldu.

---
+++ UnitTestRealWorld.Web/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitTestRealWorld.Web.Models;
+
+namespace UnitTestRealWorld.Web.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly UnitTestDbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(UnitTestDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<TEntity>();
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+    }
+}
