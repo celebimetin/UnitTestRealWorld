abe49c6b6c7d00082c6fb4dde4af7ca27b0e66ad
Metin Çelebi <metincelebi5534@gmail.com>
2023-01-31 11:18:23 +0300

    Add project files.

---
+++ UnitTestRealWorld.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UnitTestRealWorld.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
